d25b677216e9611bf28f4d49d76a55d860463b43
jschlunk <93251498+jschlunk@users.noreply.github.com>
2023-09-02 15:41:42 +0200

    first commit

---
+++ src/main/java/dev/janschlunken/contentcalender/Application.java
@@ -0,0 +1,13 @@
+package dev.janschlunken.contentcalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalenderApplication.class, args);
+	}
+
+}
